7c523cdb4e940b60c0bee568850461219afe4453
cbellmont <cbellmont@be-bound.com>
2019-01-30 08:31:40 +0100

    First commit

---
+++ src/main/java/com/bebound/template/MainApplication.java
@@ -0,0 +1,33 @@
+package com.bebound.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class MainApplication {
+
+    /**
+     * The default port with Spring is 8080: please consider to modify it in a real project
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+        Logger.getAnonymousLogger().info("SERVER IS LAUNCHED");
+
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("DESede");
+            keyGenerator.init(168);
+            SecretKey secretKey = keyGenerator.generateKey();
+            Logger.getAnonymousLogger().info("secretKey = " + secretKey.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
